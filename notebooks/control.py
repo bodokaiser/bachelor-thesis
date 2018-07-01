75d12ef340e298464c3a17a2a92f57751e34fc48
Bodo Kaiser <i@bodokaiser.io>
2018-07-01 12:29:01 +0200

    updated notebooks

--- notebooks/control.py
+++ notebooks/control.py
@@ -2,12 +2,21 @@ import numpy as np
 import requests
 
 
-def update(id, name, frequency):
+def update(id, name, frequency, nodwells=[True, True], duration=2):
   if isinstance(frequency, np.ndarray):
+    mode = 'const'
     value = 100e6
+    limits = [0, 0]
     data = frequency.tolist()
+  elif isinstance(frequency, list):
+    mode = 'sweep'
+    limits = frequency
+    value = 100e6
+    data = []
   else:
+    mode = 'playback'
     value = frequency
+    limits = [0, 0]
     data = []
 
   headers = {'Content-Type': 'application/json'}
@@ -17,7 +26,10 @@ def update(id, name, frequency):
       'amplitude': {'mode': 'const', 'value': 1},
       'phase': {'mode': 'const', 'value': 0},
       'frequency': {
+          'mode': mode,
           'const': {'value': value},
+          'sweep': {'nodwells': nodwells, 'limits': limits,
+                    'duration': duration},
           'playback': {'trigger': False, 'duplex': False,
                        'interval': 1e-5, 'data': data}
       }
@@ -30,9 +42,9 @@ def update(id, name, frequency):
   return response
 
 
-def vertical(frequency):
-  return update(9, 'Bodo Vertical', frequency)
+def vertical(frequency, **kargs):
+  return update(9, 'Bodo Vertical', frequency, **kargs)
 
 
-def horizontal(frequency):
-  return update(8, 'Bodo horizontal', frequency)
+def horizontal(frequency, **kargs):
+  return update(8, 'Bodo horizontal', frequency, **kargs)
--- notebooks/read.py
+++ notebooks/read.py
@@ -1,7 +1,28 @@
+import os
+import h5py as h5
 import numpy as np
+import pandas as pd
 
 from PIL import Image
 
 
 def image(filename):
   return np.array(Image.open(filename))[:, :, 0]
+
+
+def timeseries(path):
+  data = []
+
+  filenames = [f for f in os.listdir(path) if f.endswith('.h5')]
+  filenames.sort()
+
+  for filename in filenames:
+    basename = filename.split('.')[0]
+
+    with h5.File(os.path.join(path, filename), 'r') as file:
+      data.append({
+          'datetime': basename,
+          'voltage': file['Waveforms']['Channel 1']['Channel 1 Data'][:].mean()
+      })
+
+  return pd.DataFrame(data)
