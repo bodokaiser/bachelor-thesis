2b15f9c4f30238ae7ab864c767ba213d4ed1602d
Bodo Kaiser <i@bodokaiser.io>
2018-07-20 11:49:04 +0200

    added scope class to measurement/control.py

--- measurement/control.py
+++ measurement/control.py
@@ -1,5 +1,52 @@
 import numpy as np
 import requests
+import visa
+
+resource_manager = visa.ResourceManager()
+
+
+class MSOX6004A:
+
+  def __init__(self, hostname, timeout=5000):
+    self.resource = resource_manager.open(f'TCPIP0::{hostname}::inst0::INSTR')
+    self.resource.timeout = timeout
+    self.resource.clear()
+
+  def holla(self):
+    return self.resource.query(':*IDN?')
+
+  def single(self):
+    return self.resource.write(':SINGle')
+
+  def save(self, filename):
+    return self.resource.write(f':SAVE:WMEMory:STARt "\\usb\\{filename}.h5"')
+
+  def capture(self, channel):
+    self.resource.write(':WAVeform:FORMat WORD')
+    self.resource.write(':WAVeform:BYTeorder LSBFirst')
+    self.resource.write(':WAVeform:UNSigned 0')
+    self.resource.write(f':WAVeform:SOURce CHANnel{channel}')
+
+    values = self.resource.query_binary_values(
+        ':WAVeform:DATA?', datatype='h', container=np.array, header_fmt='ieee')
+
+    xorg = self.resource.query_ascii_values(':WAVeform:XORigin?')[0]
+    xinc = self.resource.query_ascii_values(':WAVeform:XINcrement?')[0]
+    yorg = self.resource.query_ascii_values(':WAVeform:YORigin?')[0]
+    yinc = self.resource.query_ascii_values(':WAVeform:YINcrement?')[0]
+    yref = self.resource.query_ascii_values(':WAVeform:YREference?')[0]
+
+    U = yorg + yinc * (values - yref)
+    t = np.arange(xorg, xorg + xinc * len(U), xinc)
+
+    return t, U
+
+
+def trigger():
+  response = requests.get('http://172.22.22.35:6200')
+  response.raise_for_status()
+
+  return response
 
 
 def update(id, name, frequency, amplitude=1.0, nodwells=[True, True],
